0c128836a4de4b88a3151748ad834b7ef223112d
Ernie3 <ernestgwilsoniii@gmail.com>
2015-10-25 19:53:01 -0400

    Fix up home screen a bit

--- js/countdown.js
+++ js/countdown.js
@@ -10,67 +10,67 @@ var timer;
 function showRemaining() {
     var now = new Date();
     var offset = end - now;
-    
+
     if (offset < 0) {
         clearInterval(timer);
         document.getElementById('countdown').innerHTML = '';
         return;
     }
-    
+
     var months = Math.floor(offset / _month);
-    
+
     // Adding 1 day to temp fix the month problem, since
     // months have different number of days. However 10/25/15 to
     // 02/26/16 only has one month with 30 days.
     var days = Math.floor((offset % _month) / _day) + 1;
-    
+
     var hours = Math.floor((offset % _day) / _hour);
     var minutes = Math.floor((offset % _hour) / _minute);
     var seconds = Math.floor((offset % _minute) / _second);
-    
+
     var m = "";
     var d = "";
     var h = "";
     var min = "";
     var s = "";
-    
+
     if(months > 1) {
         m = months + ' months ';
     }
     else if(months === 1) {
         m = months + ' month ';
     }
-    
+
     if(days > 1) {
         d = days + ' days ';
     }
     else if(days === 1) {
         d = days + ' day ';
     }
-    
+
     if(hours > 1) {
         h = hours + ' hours ';
     }
     else if(hours === 1) {
         h = hours + ' hour ';
     }
-    
+
     if(minutes > 1 || minutes === 0) {
         min = minutes + ' minutes ';
     }
     else if(minutes === 1) {
         min = minutes + ' minute ';
     }
-    
+
     if(seconds > 1 || seconds === 0) {
         s = seconds + ' seconds'
     }
     else if(seconds === 1) {
         s = seconds + ' second'
     }
-    
+
     var output = m + d + h + min + s;
-    
+
     document.getElementById('countdown').innerHTML = output;
 }
 
