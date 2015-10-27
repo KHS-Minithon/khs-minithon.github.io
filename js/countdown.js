64f037615da4fff6653d0828ae0982da172cfa6c
Ernie3 <ernestgwilsoniii@gmail.com>
2015-10-27 18:40:29 -0400

    Adjust timer

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,6 @@ var _second = 1000;
 var _minute = _second * 60;
 var _hour = _minute * 60;
 var _day = _hour * 24;
-var _month = _day * 31; // TODO: compensate for 28, 29, 30 and 31 day months
 var timer;
 
 function showRemaining() {
@@ -17,29 +16,15 @@ function showRemaining() {
         return;
     }
 
-    var months = Math.floor(offset / _month);
-
-    // Adding 1 day to temp fix the month problem, since
-    // months have different number of days. However 10/25/15 to
-    // 02/26/16 only has one month with 30 days.
-    var days = Math.floor((offset % _month) / _day) + 1;
-
+    var days = Math.floor((offset / _day));
     var hours = Math.floor((offset % _day) / _hour);
     var minutes = Math.floor((offset % _hour) / _minute);
     var seconds = Math.floor((offset % _minute) / _second);
 
-    var m = "";
     var d = "";
     var h = "";
     var min = "";
     var s = "";
-
-    if(months > 1) {
-        m = months + ' months ';
-    }
-    else if(months === 1) {
-        m = months + ' month ';
-    }
 
     if(days > 1) {
         d = days + ' days ';
@@ -69,7 +54,7 @@ function showRemaining() {
         s = seconds + ' second'
     }
 
-    var output = m + d + h + min + s;
+    var output = d + h + min + s;
 
     document.getElementById('countdown').innerHTML = output;
 }
