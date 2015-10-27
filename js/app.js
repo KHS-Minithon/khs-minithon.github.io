a94e8ff1b84c5a61c6e297a4b45150767f1e1997
Ernie3 <ernestgwilsoniii@gmail.com>
2015-10-27 19:25:59 -0400

    Remove Node; Remove Email stuff; Remove duplicate fonts

--- js/app.js
+++ js/app.js
@@ -1,63 +1,6 @@
 $(document).ready(function() {
 
-	// Get Money Raised Data and Adjust Progress Bar
-	
-
-	// Validate Input Fields and Send Email
-	$("#send_email").click(function() {
-
-		event.preventDefault();
-
-		var name = $("#name").val();
-		var email = $("#email").val();
-		var committe = $("#committe").val();
-		var subject = $("#subject").val();
-		var message  = $("#message").val();
-
-		if(name === '' || name.length === 0) {
-
-			alert("Name is required.");
-			$("#name").focus();
-            return false;
-		}
-
-		if(email === '' || email.length === 0) {
-
-			alert("Email is required.");
-			$("#email").focus();
-            return false;
-		}
-
-		if(subject === '' || subject.length === 0) {
-
-			alert("Subject is required.");
-			$("#subject").focus();
-            return false;
-		}
-
-		if(message === '' || message.length === 0) {
-
-			alert("Message is required.");
-			$("#message").focus();
-            return false;
-		} else {
-
-			$.post("http://localhost:3000/email",
-				{ name: name,
-				  email: email,
-				  committe: committe,
-				  subject: subject,
-				  message: message
-				}
-			).done(function() {
-
-				//Confirmation takes too long and we can't wait..time for a hail mary play
-				alert("Email was sent! Thank you.");
-				$('#contact_form').trigger("reset");
-
-			});
-		}
-	});
+	// TODO: Get Money Raised Data and Adjust Progress Bar
 
   // Image Gallery Initiation and Settings
   // **VIDEO POP-UP ONLY WORK IN A SERVER**
