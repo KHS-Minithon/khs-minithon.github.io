ed005bfe614608576f1841d55b96e93453d25a87
Ernie3 <ernestgwilsoniii@gmail.com>
2015-10-27 23:47:40 -0400

    yeah travis messing up

--- test/spec.js
+++ test/spec.js
@@ -103,7 +103,7 @@ casper.test.begin('Important Links', function suite(test) {
 
         // Navigation Bar
         test.assertExists('.navbar-nav', 'Navigation Bar Found');
-        test.assertElementCount('.navbar-nav li', 8, '8 Sections in Navigation Bar Found');
+        test.assertElementCount('.navbar-nav li', 7, '7 Sections in Navigation Bar Found');
 
             // Home
             test.assertEquals(this.fetchText('.navbar-nav li:nth-child(1) a'), "Home", "Correct Navigation-Home Text");
