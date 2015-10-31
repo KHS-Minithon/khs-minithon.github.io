575f1bf2b05a7bbb9468c746d888b4e5776f36aa
Ernie3 <ernestgwilsoniii@gmail.com>
2015-10-31 16:19:41 -0400

    Someday we will have email... some day...

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 	// Get Money Raised Data and Adjust Progress Bar
+	// Spreadsheet: https://docs.google.com/spreadsheets/d/1PE1umwfQwSwaNxX_Hishe8kKmcNPRQmRBv-GrPrBFDw/pubhtml
 	var khs_spreadsheet = "https://spreadsheets.google.com/feeds/list/1PE1umwfQwSwaNxX_Hishe8kKmcNPRQmRBv-GrPrBFDw/od6/public/basic?alt=json";
 
 	$.getJSON(khs_spreadsheet).done(function(data) {
@@ -29,61 +30,6 @@ $(document).ready(function() {
 		var status = textStatus + ", " + error;
 		console.error("Request Failed: " + status );
 
-	});
-
-	// Validate Input Fields and Send Email
-	$("#send_email").click(function() {
-
-		event.preventDefault();
-
-		var name = $("#name").val();
-		var email = $("#email").val();
-		var committe = $("#committe").val();
-		var subject = $("#subject").val();
-		var message  = $("#message").val();
-
-		if(name === '' || name.length === 0) {
-			alert("Name is required.");
-			$("#name").focus();
-            return false;
-		}
-
-		if(email === '' || email.length === 0) {
-
-			alert("Email is required.");
-			$("#email").focus();
-            return false;
-		}
-
-		if(subject === '' || subject.length === 0) {
-
-			alert("Subject is required.");
-			$("#subject").focus();
-            return false;
-		}
-
-		if(message === '' || message.length === 0) {
-
-			alert("Message is required.");
-			$("#message").focus();
-            return false;
-		} else {
-
-			$.post("server/email.php",
-				{ name: name,
-				  email: email,
-				  committe: committe,
-				  subject: subject,
-				  message: message
-				}
-			).done(function() {
-
-				// Confirmation takes too long and we can't wait..time for a hail mary play
-				alert("Email was sent! Thank you.");
-				$('#contact_form').trigger("reset");
-
-			});
-		}
 	});
 
   // Image Gallery Initiation and Settings
