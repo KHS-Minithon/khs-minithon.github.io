709f0b4a10446cb8b920681f8249bb85083922ba
JoseRoman <jose_roman32@hotmail.com>
2015-10-30 19:41:14 -0700

    fixed GA bounce rate?....maybe

--- js/theme.js
+++ js/theme.js
@@ -7,6 +7,10 @@
 
 $('.page-scroll').bind('click', function(event) {
     var $anchor = $(this);
+
+    //Google Analytics Track Tabs
+    ga('send', 'event', $anchor.text(), 'clicked');
+    
     $('html, body').stop().animate({
         scrollTop: $($anchor.attr('href')).offset().top -64
     }, 1500, 'easeInOutExpo');
