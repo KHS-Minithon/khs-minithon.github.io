e23da7ed135d47c05c653529ffea9be769d47d23
michaelbellino <michael.j.bellino123@gmail.com>
2016-11-23 11:32:49 -0500

    hate myself :)

--- js/app.js
+++ js/app.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
 	// Spreadsheet: https://docs.google.com/spreadsheets/d/1PE1umwfQwSwaNxX_Hishe8kKmcNPRQmRBv-GrPrBFDw/pubhtml & https://spreadsheets.google.com/feeds/list/1PE1umwfQwSwaNxX_Hishe8kKmcNPRQmRBv-GrPrBFDw/od6/public/basic?alt=json
 	var khs_spreadsheet = "https://spreadsheets.google.com/feeds/list/1PE1umwfQwSwaNxX_Hishe8kKmcNPRQmRBv-GrPrBFDw/od6/public/basic/d9ney";
 
-	  $.ajax({
-    url: "https://spreadsheets.google.com/feeds/list/1PE1umwfQwSwaNxX_Hishe8kKmcNPRQmRBv-GrPrBFDw/od6/public/basic/d9ney",
-    type: "GET",
-    dataType: "xml",
-	success: function(data) {
-        	 var xmlDoc = $.parseXML(data),
- 		 $xmlDoc = $(xmlDoc),
-  		xml = $xmlDoc.find("<content type='text'>");
-		var money_raised = xml
-
-		
-				}
-	
-        	
-           
-    );
-	
+//	  $.ajax({
+//    url: "https://spreadsheets.google.com/feeds/list/1PE1umwfQwSwaNxX_Hishe8kKmcNPRQmRBv-GrPrBFDw/od6/public/basic/d9ney",
+//    type: "GET",
+//    dataType: "xml",
+//	success: function(data) {
+ //       	 var xmlDoc = $.parseXML(data),
+ //		 $xmlDoc = $(xmlDoc),
+  //		xml = $xmlDoc.find("<content type='text'>");
+//		var money_raised = xml
+//
+//		
+//				}
+//	
+  //      	
+    //       
+   // );
+		var money_raised = 760;
 		var goal = 65000;
 		var current_money = (money_raised / goal) * 100;
 
