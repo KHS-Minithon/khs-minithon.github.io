989160f27db6d4d5e454cb072871cce7490c04b4
michaelbellino <michaelbellino@michaels-mbp.kcsd.org>
2017-02-13 12:25:58 -0500

    Updated total

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         	
            
     
-		var money_raised = 15458;
+		var money_raised = 17168;
 		var goal = 65000;
 		var current_money = (money_raised / goal) * 100;
 		var current_moneyrounded = Math.floor(current_money);
