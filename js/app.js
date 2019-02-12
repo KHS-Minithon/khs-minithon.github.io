807ce2773638d900bebf891344fb4e7700144b18
willmichael123 <33104757+willmichael123@users.noreply.github.com>
2019-02-11 21:33:49 -0500

    moneybar 10695

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         	
            
     
-		var money_raised = 7309;
+		var money_raised = 10695;
 		var goal = 40000;
 		var current_money = (money_raised / goal) * 100;
 		var current_moneyrounded = Math.floor(current_money);
